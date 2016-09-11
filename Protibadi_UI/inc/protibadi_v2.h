bf1af57beda77afa7f68c5d42e2f4daa79f3c647
Asimuzzaman <mansib.raj@gmail.com>
2016-09-11 14:23:48 +0600

    Add initial code [Basic UI app]

---
+++ Protibadi_UI/inc/protibadi_v2.h
@@ -0,0 +1,19 @@
+#ifndef __protibadi_v2_H__
+#define __protibadi_v2_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "protibadi_v2"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.protibadi_v2"
+#endif
+
+#endif /* __protibadi_v2_H__ */
