1bc6a5448ba8df085a870839dec18692f2bbc18d
asimuzzaman <mansib.raj@gmail.com>
2018-12-03 23:07:43 +0600

    Update service message port

--- Protibadi_service/src/message_port/messaging_port.c
+++ Protibadi_service/src/message_port/messaging_port.c
@@ -15,19 +15,31 @@ message_port_cb(int local_port_id, const char *remote_app_id, const char *remote
     bundle_get_str(message, "command", &command);
     //bundle_get_str(message, "data", &data);
 
-    if(strcmp(command, "HELP") == 0) //"Ask for help" button call back
-    	run_app();
-
     dlog_print(DLOG_INFO, LOG_TAG, "Message from %s, command: %s ",
                remote_app_id, command);
+
+    if(strcmp(command, "HELP") == 0) //"Ask for help" button call back
+        run_app(); //calling Data Processor function
+
+    else if(strcmp(command,"ADD_CONTACT") == 0) { //adding contact in the database
+        char *name = NULL, *number = NULL, *email = NULL;
+
+        bundle_get_str(message, "name", &name);
+        bundle_get_str(message, "number", &number);
+        bundle_get_str(message, "email", &email);
+
+        dlog_print(DLOG_DEBUG,LOG_TAG,"Received: %s, %s, %s.",name,number,email);
+        add_contact(name, number, email);
+    }
+
 }
 
 void message_receiver() {
-	char *local_port = "test1"; //NOT SURE
+    char *local_port = "test1"; //port for UI app
 
-	int port_id = message_port_register_local_port(local_port, message_port_cb, NULL);
-	if (port_id < 0)
-	    dlog_print(DLOG_ERROR, LOG_TAG, "Port register error: %d", port_id);
-	else
-	    dlog_print(DLOG_INFO, LOG_TAG, "port_id: %d", port_id);
+    int port_id = message_port_register_local_port(local_port, message_port_cb, NULL);
+    if (port_id < 0)
+        dlog_print(DLOG_ERROR, LOG_TAG, "Port register error: %d", port_id);
+    else
+        dlog_print(DLOG_INFO, LOG_TAG, "port_id: %d", port_id);
 }
